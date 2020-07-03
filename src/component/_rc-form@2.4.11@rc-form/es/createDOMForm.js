08beaad1abfda74b23945b0dc4fb7d532d69dd98
姚观寿 <yaoguanshou@rainbowcn.com>
2020-07-03 18:00:04 +0800

    1

--- src/component/_rc-form@2.4.11@rc-form/es/createDOMForm.js
+++ src/component/_rc-form@2.4.11@rc-form/es/createDOMForm.js
@@ -1,63 +1,75 @@
-import _extends from 'babel-runtime/helpers/extends';
-import ReactDOM from 'react-dom';
-import scrollIntoView from 'dom-scroll-into-view';
-import has from 'lodash/has';
-import createBaseForm from './createBaseForm';
-import { mixin as formMixin } from './createForm';
-import { getParams } from './utils';
+import _extends from "babel-runtime/helpers/extends";
+import ReactDOM from "react-dom";
+import scrollIntoView from "dom-scroll-into-view";
+import has from "lodash/has";
+import createBaseForm from "./createBaseForm";
+import { mixin as formMixin } from "./createForm";
+import { getParams } from "./utils";
 
+// 获取样式属性值
 function computedStyle(el, prop) {
   var getComputedStyle = window.getComputedStyle;
   var style =
-  // If we have getComputedStyle
-  getComputedStyle ?
-  // Query it
-  // TODO: From CSS-Query notes, we might need (node, null) for FF
-  getComputedStyle(el) :
-
-  // Otherwise, we are in IE and use currentStyle
-  el.currentStyle;
+    // If we have getComputedStyle
+    getComputedStyle
+      ? // Query it
+        // TODO: From CSS-Query notes, we might need (node, null) for FF
+        getComputedStyle(el)
+      : // Otherwise, we are in IE and use currentStyle
+        el.currentStyle;
   if (style) {
+    //获取样式属性值
     return style[
-    // Switch to camelCase for CSSOM
-    // DEV: Grabbed from jQuery
-    // https://github.com/jquery/jquery/blob/1.9-stable/src/css.js#L191-L194
-    // https://github.com/jquery/jquery/blob/1.9-stable/src/core.js#L593-L597
-    prop.replace(/-(\w)/gi, function (word, letter) {
-      return letter.toUpperCase();
-    })];
+      // Switch to camelCase for CSSOM
+      // DEV: Grabbed from jQuery
+      // https://github.com/jquery/jquery/blob/1.9-stable/src/css.js#L191-L194
+      // https://github.com/jquery/jquery/blob/1.9-stable/src/core.js#L593-L597
+      prop.replace(/-(\w)/gi, function (word, letter) {
+        // 比如 margin-left 变成 marginLeft
+        return letter.toUpperCase(); // 变成大写
+      })
+    ];
   }
   return undefined;
 }
 
+//得到可滚动的容器
 function getScrollableContainer(n) {
   var node = n;
   var nodeName = void 0;
   /* eslint no-cond-assign:0 */
-  while ((nodeName = node.nodeName.toLowerCase()) !== 'body') {
-    var overflowY = computedStyle(node, 'overflowY');
+  while ((nodeName = node.nodeName.toLowerCase()) !== "body") {
+    // 获取样式属性值
+    var overflowY = computedStyle(node, "overflowY");
     // https://stackoverflow.com/a/36900407/3040605
-    if (node !== n && (overflowY === 'auto' || overflowY === 'scroll') && node.scrollHeight > node.clientHeight) {
+    if (
+      node !== n &&
+      (overflowY === "auto" || overflowY === "scroll") && // 如果css属性设置了 可以滚动 属性
+      node.scrollHeight > node.clientHeight //并且滚动的高 大于 可视的高 此时 会出现滚动条
+    ) {
       return node;
     }
+    // 否则一直找上一个父节点
     node = node.parentNode;
   }
-  return nodeName === 'body' ? node.ownerDocument : node;
+  // 知道父节点是body标签为止，返回dom
+  return nodeName === "body" ? node.ownerDocument : node;
 }
 
 var mixin = {
+  // 获取form 表单
   getForm: function getForm() {
     return _extends({}, formMixin.getForm.call(this), {
-      validateFieldsAndScroll: this.validateFieldsAndScroll
+      validateFieldsAndScroll: this.validateFieldsAndScroll,
     });
   },
   validateFieldsAndScroll: function validateFieldsAndScroll(ns, opt, cb) {
     var _this = this;
 
     var _getParams = getParams(ns, opt, cb),
-        names = _getParams.names,
-        callback = _getParams.callback,
-        options = _getParams.options;
+      names = _getParams.names,
+      callback = _getParams.callback,
+      options = _getParams.options;
 
     var newCb = function newCb(error, values) {
       if (error) {
@@ -71,7 +83,10 @@ var mixin = {
             if (instance) {
               var node = ReactDOM.findDOMNode(instance);
               var top = node.getBoundingClientRect().top;
-              if (node.type !== 'hidden' && (firstTop === undefined || firstTop > top)) {
+              if (
+                node.type !== "hidden" &&
+                (firstTop === undefined || firstTop > top)
+              ) {
                 firstTop = top;
                 firstNode = node;
               }
@@ -81,19 +96,26 @@ var mixin = {
 
         if (firstNode) {
           var c = options.container || getScrollableContainer(firstNode);
-          scrollIntoView(firstNode, c, _extends({
-            onlyScrollIfNeeded: true
-          }, options.scroll));
+          scrollIntoView(
+            firstNode,
+            c,
+            _extends(
+              {
+                onlyScrollIfNeeded: true,
+              },
+              options.scroll
+            )
+          );
         }
       }
 
-      if (typeof callback === 'function') {
+      if (typeof callback === "function") {
         callback(error, values);
       }
     };
 
     return this.validateFields(names, options, newCb);
-  }
+  },
 };
 
 // 创建DomForm
