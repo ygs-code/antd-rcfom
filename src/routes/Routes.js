665698d247f6a1789105da901752153829048b91
江熠平 <jiangyiping@rainbowcn.com>
2021-06-25 17:04:04 +0800

    一对多拖拽

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -6,48 +6,44 @@
  * @Description: In User Settings Edit
  * @FilePath: /antd-rcfom/src/routes/Routes.js
  */
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import '~antd/dist/antd.css';
 
-const Home = lazy(() => import('../pages/Home/Home'));
-const About = lazy(() => import('../pages/About/About'));
-const Antd = lazy(() => import('../pages/Antd/Antd'));
-const Form = lazy(() => import('../pages/Form/Form'));
-const RcForm = lazy(() => import('../pages/RcForm/RcForm'));
-const LogicFlow = lazy(() => import('../pages/LogicFlow'));
-const Go = lazy(() => import('../pages/Go'));
-const DragDrop = lazy(() => import('../pages/DragDrop'));
-const IVRtree = lazy(() => import('../pages/IVRtree'));
-const DraggableLink = lazy(() => import('../pages/DraggableLink'));
-const ReactDraggableLink = lazy(() => import('../pages/ReactDraggableLink'));
- 
- 
-
- 
+const Home = lazy(() => import("../pages/Home/Home"));
+const About = lazy(() => import("../pages/About/About"));
+const Antd = lazy(() => import("../pages/Antd/Antd"));
+const Form = lazy(() => import("../pages/Form/Form"));
+const RcForm = lazy(() => import("../pages/RcForm/RcForm"));
+const LogicFlow = lazy(() => import("../pages/LogicFlow"));
+const Go = lazy(() => import("../pages/Go"));
+const DragDrop = lazy(() => import("../pages/DragDrop"));
+const IVRtree = lazy(() => import("../pages/IVRtree"));
+const DraggableLink = lazy(() => import("../pages/DraggableLink"));
+const ReactDraggableLink = lazy(() => import("../pages/ReactDraggableLink"));
+const ReactGoJS = lazy(() => import("../pages/ReactGoJS"));
 
 function Routes() {
-    return (
-        <Router>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/about" component={About} />
-                    <Route path="/antd" component={Antd} />
-                    <Route path="/form" component={Form} />
-                    <Route path="/rcForm" component={RcForm} />
-                    <Route path="/LogicFlow" component={LogicFlow} />
-                    <Route path="/Go" component={Go} />
-                    <Route path="/DragDrop" component={DragDrop} />
-                    <Route path="/IVRtree" component={IVRtree} />
-                    <Route path="/DraggableLink" component={DraggableLink} />
-                    <Route path="/ReactDraggableLink" component={ReactDraggableLink} />
-                
-                 
-                </Switch>
-            </Suspense>
-        </Router>
-    );
+  return (
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/antd" component={Antd} />
+          <Route path="/form" component={Form} />
+          <Route path="/rcForm" component={RcForm} />
+          <Route path="/LogicFlow" component={LogicFlow} />
+          <Route path="/Go" component={Go} />
+          <Route path="/DragDrop" component={DragDrop} />
+          <Route path="/IVRtree" component={IVRtree} />
+          <Route path="/DraggableLink" component={DraggableLink} />
+          <Route path="/ReactDraggableLink" component={ReactDraggableLink} />
+          <Route path="/ReactGoJS" component={ReactGoJS} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default Routes;
